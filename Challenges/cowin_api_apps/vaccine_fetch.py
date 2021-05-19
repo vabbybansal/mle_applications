47744e0563a8bced703e7ee869030ff0ac8ed868
vabbybansal <vabby.bansal@gmail.com>
2021-05-20 02:55:43 +0530

    Update vaccine_fetch.py
    Changed API to calendarByDistrict which returns weekly vaccine slots

--- Challenges/cowin_api_apps/vaccine_fetch.py
+++ Challenges/cowin_api_apps/vaccine_fetch.py
@@ -8,7 +8,7 @@ from os import path
 import logging
 
 # CONSTANTS
-URLSTRING_VACCINE_BY_DISTRICT = "https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/findByDistrict?district_id={district_id}&date={datestr}" 
+URLSTRING_VACCINE_BY_DISTRICT = "https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/calendarByDistrict?district_id={district_id}&date={datestr}" 
 CSV_FILE_NAME = '/Users/vaibhavb/Desktop/repos/dump/mle_applications/Challenges/cowin_api_apps/vaccine_availability.csv'
 
 dir_path = os.path.dirname(os.path.realpath(__file__))
@@ -51,26 +51,29 @@ def get_vaccine_metrics_by_district(district_id, datestr):
 			headers
 		).json()
 	
-	num_sites = len(response_dict_json['sessions'])
+	total_capacity = 0
 	num_vaccine_type = 0
 	vaccine_type_freq = {}
 
-	if num_sites > 0:
-		for site in response_dict_json['sessions']:
-			vaccine_type = site['vaccine']
-			if vaccine_type not in vaccine_type_freq:
-				vaccine_type_freq[vaccine_type] = 0
-			vaccine_type_freq[vaccine_type] += 1
-		num_vaccine_type = len(vaccine_type_freq)
+	for center in response_dict_json['centers']:
 
-	else: pass
+		sessions = center['sessions']
+		for session in sessions:
+			session_capacity = session['available_capacity']
+			total_capacity += session_capacity
+			if session_capacity > 0:
+				vaccine_type = session['vaccine']
+				if vaccine_type not in vaccine_type_freq:
+					vaccine_type_freq[vaccine_type] = 0
+				vaccine_type_freq[vaccine_type] += session_capacity
+		num_vaccine_type = len(vaccine_type_freq)
 
 	return [
 		datetime.datetime.now(),
 		datestr,
 		district_id,
 		DISTRICT_ID_MAP[district_id],
-		num_sites,
+		total_capacity,
 		num_vaccine_type,
 		json.dumps(vaccine_type_freq)
 	]
@@ -80,7 +83,7 @@ def write_row_vaccine_by_district(data_row):
 	if (path.exists(CSV_FILE_NAME) == False):
 		with open(CSV_FILE_NAME, 'w', newline='') as file_object:
 		    writer = csv.writer(file_object)
-		    writer.writerow(["timestamp", "datestr_vaccine_for", "district_id", "district_name", "num_sites", "num_vaccine_types", "vaccine_type_freq_json"])
+		    writer.writerow(["timestamp", "datestr_vaccine_for", "district_id", "district_name", "total_capacity", "num_vaccine_types", "vaccine_type_freq_json"])
 		    file_object.close()
 
 	with open(CSV_FILE_NAME, 'a', newline='') as file_object:
@@ -91,8 +94,10 @@ def write_row_vaccine_by_district(data_row):
 
 def main():
 	do_logging("started")
-	for city_date_tuple in [(DISTRICT_MAP['New_Delhi'], '25-05-2021'), (DISTRICT_MAP['Gurgaon'], '25-05-2021')]:
+	today_datestr = (datetime.date.today() + datetime.timedelta(days=0)).strftime("%d-%m-%Y")
+	for city_date_tuple in [(DISTRICT_MAP['New_Delhi'], today_datestr), (DISTRICT_MAP['Gurgaon'], today_datestr)]:
 		data_row = get_vaccine_metrics_by_district(city_date_tuple[0], city_date_tuple[1])
+		print(data_row)
 		write_row_vaccine_by_district(data_row)
 	do_logging("exiting")
 main()
